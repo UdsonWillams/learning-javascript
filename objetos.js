d64cef46b8712f92acb078a3b8ec3a35009398ae
UdsonWillams <udson.willams@gmail.com>
2023-11-15 01:42:08 -0300

    Criando objetos JavaScript

---
+++ objetos.js
@@ -0,0 +1,9 @@
+// Sintaxe de objetos JS
+
+let pessoa_ex_vazio = {}; // Objeto vazio
+
+let pessoa = { // Objeto do tipo pessoa
+    nome: "Udson",
+    sobrenome: "Willams",
+    idade: 27,    
+};
