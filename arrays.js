fa1ebd3d285960f12830892164e64261cbf2bcd6
UdsonWillams <udson.willams@gmail.com>
2023-11-15 01:41:00 -0300

    Entendendo Arrays em js

---
+++ arrays.js
@@ -0,0 +1,13 @@
+// Arrays
+// Conjunto de dados que pode ser acessado por um indice. 
+
+let familia = [
+    26, 45, 50, 17
+];
+
+let array_com_varios_valores = [
+    true, "teste", "estou no array", 45, 17.5
+];
+// Mesma logica de outras linguaguens
+// Indice começa no 0, possui um atributo lenght
+// Acessado pelo indice, ex.: familia[0]
