a8336715ed014484c131a3f7f7625b4456d02ba3
UdsonWillams <udson.willams@gmail.com>
2024-04-15 17:53:17 -0300

    make mysql connection in the project.

--- Curso_NODEJS/api-rest/server.js
+++ Curso_NODEJS/api-rest/server.js
@@ -1,7 +1,16 @@
 import app from './src/app.js'
+import connection from './infra/conexion.js'
+
 const PORT = 3000 // porta do servidor local.
 
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando: http://localhost:${PORT}`)
+// make connection
+connection.connect((error) =>{
+    if (error){
+        console.log(error)
+    } else {
+        console.log("conexão com banco realizada!")
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando: http://localhost:${PORT}`)
+        })
+    }
 })
