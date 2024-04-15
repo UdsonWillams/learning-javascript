4bb9b9198f010d0e35af5a59311a7ab60388de08
UdsonWillams <udson.willams@gmail.com>
2024-04-15 20:51:45 -0300

    routes connecting in db - mysql

--- Curso_NODEJS/api-rest/src/app.js
+++ Curso_NODEJS/api-rest/src/app.js
@@ -1,56 +1,72 @@
 import express from 'express' // importando o express atravez do require para o projeto
-const app = express() // cria a instancia do express no projeto. iniciando o novo app.
+import connection from '../infra/connection.js'
+
+// cria a instancia do express no projeto. iniciando o novo app.
+const app = express()
 
 // Indicar para o express ler o body passado com json
 app.use(express.json())
 
-// data mock
-const selecoes = [
-    {id: 1, selecao: "Brasil", grupo: "G"},
-    {id: 2, selecao: "Suiça", grupo: "G"},
-    {id: 3, selecao: "Sérvia", grupo: "G"},
-    {id: 4, selecao: "Camarões", grupo: "G"}
-]
 
-function searchCountryById(id){
-    return selecoes.filter(selecao => selecao.id == id)
-}
-
-function searchCountryIndex(id){
-    return selecoes.findIndex(selecao => selecao.id == id)
-}
-
-// criar rota raiz.
-app.get('/', (req, resp) => {
-    resp.send("Hello World!")
+//ROUTES
+app.get("/countries", (req, resp) => {
+    const sql = "SELECT * FROM countries;"
+    connection.query(sql, (error, result) => {
+        if (error){
+            resp.status(404).json({"error": error})
+        } else {
+            resp.status(200).json(result)
+        }
+    })
 })
 
-app.get("/selecoes", (req, resp) => {
-    resp.status(200).send(selecoes)
+app.get("/countries/:id", (req, resp) => {
+    const id = req.params.id
+    const sql = "SELECT * FROM countries WHERE id=?;"
+    connection.query(sql, id, (error, result) => {
+        if (error){
+            resp.status(404).json({"error": error})
+        } else {
+            resp.status(200).json(result)
+        }
+    })
 })
 
-app.get("/selecoes/:id", (req, resp) => {
-    resp.json(searchCountryById(req.params.id))
+app.post('/countries', (res, resp) => {
+    const country_data = req.body
+    const sql = "INSERT INTO countries SET ?;"
+    connection.query(sql, country_data, (error, result) => {
+        if (error){
+            resp.status(400).json({"error": error})
+        } else {
+            resp.status(201).json(result)
+        }
+    })
 })
 
-app.post('/selecoes', (res, resp) => {
-    selecoes.push(res.body)
-    resp.status(201).send({status: "success"})
+app.delete("/countries/:id", (req, resp) => {
+    const id = req.params.id
+    const sql = "DELETE FROM countries WHERE id=?;"
+    connection.query(sql, id, (error, result) => {
+        if (error){
+            resp.status(404).json({"error": error})
+        } else {
+            resp.status(200).json(result)
+        }
+    })
 })
 
-app.delete("/selecoes/:id", (req, resp) => {
-    let index = searchCountryIndex(req.params.id)
-    // splice apaga o valor do index e mais quantos 
-    // valores nos queremos apagar a partir daquele index.
-    selecoes.splice(index, 2)
-    resp.status(200).json({status: "success"})
-})
-
-app.put("/selecoes/:id", (req, resp) => {
-    let index = searchCountryIndex(req.params.id)
-    selecoes[index].selecao = req.body.selecao
-    selecoes[index].grupo = req.body.grupo
-    resp.status(200).json(selecoes)
+app.put("/countries/:id", (req, resp) => {
+    const id = req.params.id
+    const country_data = req.body
+    const sql = "UPDATE countries SET ? WHERE id=?;"
+    connection.query(sql, [country_data, id], (error, result) => {
+        if (error){
+            resp.status(400).json({"error": error})
+        } else {
+            resp.status(201).json(result)
+        }
+    })
 })
 
 export default app
