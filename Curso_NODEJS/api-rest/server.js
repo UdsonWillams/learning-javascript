bd8e8ebe9234b232c918f53ced8bcb689d22b9d3
UdsonWillams <udson.willams@gmail.com>
2024-04-15 20:51:17 -0300

    refactoring server file

--- Curso_NODEJS/api-rest/server.js
+++ Curso_NODEJS/api-rest/server.js
@@ -1,16 +1,7 @@
 import app from './src/app.js'
-import connection from './infra/conexion.js'
 
 const PORT = 3000 // porta do servidor local.
 
-// make connection
-connection.connect((error) =>{
-    if (error){
-        console.log(error)
-    } else {
-        console.log("conexão com banco realizada!")
-        app.listen(PORT, () => {
-            console.log(`Servidor rodando: http://localhost:${PORT}`)
-        })
-    }
+app.listen(PORT, () => {
+    console.log(`Servidor rodando: http://localhost:${PORT}`)
 })
